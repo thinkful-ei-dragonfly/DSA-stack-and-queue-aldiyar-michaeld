4d382ba23a73d4ab6deb7e5f1ef1eee6ebf3f0c4
Michael Dusaniwskyj <dusaniwskyj@gmail.com>
2019-07-26 13:59:08 -0400

    finished parenth balance check

--- stack.js
+++ stack.js
@@ -1,3 +1,4 @@
+/* eslint-disable no-console */
 /* eslint-disable strict */
 'ust strict';
 
@@ -54,7 +55,7 @@ function display(stack) {
 }
 
 function is_palindrome(s) {
-  s = s.toLowerCase().replace(/[^a-zA-Z0-9]/g, "");
+  s = s.toLowerCase().replace(/[^a-zA-Z0-9]/g, '');
   let stack = new Stack();
 
   for (let i = 0; i < s.length; i++) {
@@ -62,7 +63,7 @@ function is_palindrome(s) {
     stack.push(char);
   }
 
-  let reversedString = ''
+  let reversedString = '';
   while (stack.top !== null) {
     reversedString += stack.top.value;
     stack.top = stack.top.next;
@@ -75,60 +76,54 @@ function is_palindrome(s) {
     console.log('String is not palindrome');
     return false;
   }
-  
 }
 
-function balanced(string) {
-  //   Input: exp = “[ () ] { }{ [ ()() ]() }”
-  //   Output: Balanced
-  //   Input: exp = “[(])”
-  //   Output: Not Balanced
+function balanced(s) {
 
   let stack = new Stack();
+  let last = '';
+  for (let i = 0; i < s.length; i++) {
+    if (s[i] === '(') {
+      console.log(`step ${i} push ${s[i]} to stack `);
+      stack.push(s[i]);
+    }
 
-  for (let i = 0; i < string.length; i++) {
-    let char = string[i];
-    if ( char === '(') {
-      stack.push(char);
-    } 
-    if ( char === ')' && stack !== null) {
-      stack.pop();
-    }
-    
-    }
-    if (isEmpty(stack)) {
-      console.log('Expression is Balanced')
-    } else {
-      console.log('Expression is NOT Balanced')
+    if (s[i] === ')') {
+      console.log(`step ${i} pop ${s[i]} from stack`);
+      last = stack.pop();
+      if (last === '(' && s[i] !== ')') {
+        return i;
+      }
     }
   }
-
+  if (stack.top !== null) {
+    console.log(`character #${s.length - 1}: has no matching closing )`);
+  } else {
+    console.log('All parentheses match');
+  }
+}
 
 function main() {
-  const starTrek = new Stack();
+  balanced('()()() (()) ()');
+  // const starTrek = new Stack();
+  // starTrek.push('Kirk');
+  // starTrek.push('Spock');
+  // starTrek.push('McCoy');
+  // starTrek.push('Scotty');
+  // starTrek.pop();
+  // starTrek.pop();
 
-  starTrek.push('Kirk');
-  starTrek.push('Spock');
-  starTrek.push('McCoy');
-  starTrek.push('Scotty');
-  starTrek.pop();
-  starTrek.pop();
-  
-
-  balanced("[()]{}{[()()]()}")
-  balanced("[(])())()")
-  
   // console.log(starTrek);
   // console.log(peek(starTrek));
   // console.log(isEmpty(starTrek));
   // display(starTrek);
-  
+
   // True, true, true, false
   // is_palindrome('lasd asfkm iwjodm ,,,,,')
-// console.log(is_palindrome("dad"));
-// console.log(is_palindrome("A man, a plan, a canal: Panama"));
-// console.log(is_palindrome("1001"));
-// console.log(is_palindrome("Tauhida"));
+  // console.log(is_palindrome("dad"));
+  // console.log(is_palindrome("A man, a plan, a canal: Panama"));
+  // console.log(is_palindrome("1001"));
+  // console.log(is_palindrome("Tauhida"));
 }
 
 main();
